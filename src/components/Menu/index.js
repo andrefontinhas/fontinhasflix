c74e71e171261f69c72e105cabc42e3b5ec1f992
Andre do Valle Fontinhas <Andre.Fontinhas@altitude.com>
2020-07-27 17:23:07 -0300

    acertando nome do site na index.html

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -8,7 +8,7 @@ function Menu() {
     return (
         <nav className="Menu">
             <a href="/" >
-                <img className="Logo" src={Logo} alt="Fontinhsflix logo"/>
+                <img className="Logo" src={Logo} alt="Fontinhasflix logo"/>
             </a>
             <Button as="a" className="ButtonLink" href="/">
                 Novo vídeo
